7108a225cf3f431860009e168d2e188a43187171
prasad7199 <138664466+prasad7199@users.noreply.github.com>
2023-08-18 20:21:35 +0530

    Update DB creation Queries.sql

--- Backend/MySQL/DB creation Queries.sql
+++ Backend/MySQL/DB creation Queries.sql
@@ -1,15 +1,13 @@
-
-
 create table user (
 user_id int auto_increment primary key,
-email_address varchar(50) unique not null,
-password varchar(50),
+email varchar(50) unique not null,
+password varchar(50) not null,
 user_name varchar(50) unique not null,
 name varchar(50),
-date_of_birth date,
+dob date,
 gender varchar(10),
-mobile_number varchar(12),
-profile_photo varchar(200),
+mobile varchar(12),
+profile_photo varchar(500),
 extra1 varchar(0),
 extra2 varchar(0),
 extra3 varchar(0),
@@ -19,7 +17,8 @@ extra4 varchar(0)
 create table post (
 post_id int auto_increment primary key,
 user_id int,
-photo_url varchar(500),
+post_url varchar(500),
+location varchar(200),
 created_datetime datetime,
 caption varchar(200),
 likes_count int,
@@ -31,7 +30,7 @@ foreign key (user_id) references user(user_id) on delete cascade on update casca
 
 
 
-create table post_likes (
+create table post_like (
 like_id int primary key auto_increment,
 post_id int,
 user_id int,
@@ -40,10 +39,9 @@ extra1 varchar(0),
 extra2 varchar(0),
 foreign key (user_id) references user(user_id) on delete cascade on update cascade,
 foreign key (post_id) references post(post_id) on delete cascade on update cascade
-
 );
 
-create table post_comments (
+create table post_comment (
 comment_id int primary key auto_increment,
 post_id int,
 user_id int,
@@ -73,10 +71,10 @@ foreign key (community_id) references community(community_id) on delete cascade 
 );
 
 
-
 create table trip (
 trip_id int primary key auto_increment,
 user_id int,
+location varchar(200),
 itinerary varchar(500),
 interested_count int,
 extra1 varchar(0),
@@ -107,6 +105,7 @@ foreign key (user_two) references user(user_id) on delete cascade on update casc
 );
 
 
+
 create table message (
 message_id int primary key auto_increment,
 user_sender int,
@@ -119,31 +118,20 @@ foreign key (user_receiver) references user(user_id) on delete cascade on update
 
 create table connection (
 connection_id int primary key auto_increment,
-user_follower int,
-user_following int,
+following_to int not null,
+followed_by int not null,
 extra1 varchar(0),
 extra2 varchar(0),
-foreign key (user_follower) references user(user_id) on delete cascade on update cascade,
-foreign key (user_following) references user(user_id) on delete cascade on update cascade
+foreign key (following_to) references user(user_id) on delete cascade on update cascade,
+foreign key (followed_by) references user(user_id) on delete cascade on update cascade
 );
 
 
 create table admin (
-admin_id int,
+admin_id int primary key auto_increment,
+user_id int,
+
 extra1 varchar(0),
 extra2 varchar(0),
-foreign key (admin_id) references user(user_id) on delete cascade on update cascade
+foreign key (user_id) references user(user_id) on delete cascade on update cascade
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
